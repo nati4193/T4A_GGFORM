aa2606991c57ae99d31d0f8c6718054b983cdcbe
nati4193 <nng.nati@gmail.com>
2020-07-14 16:54:02 +0700

    commit

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 #Import Library
 import re
 import pandas as pd
+import numpy as np
 
 print("Pandas version",pd.__version__)
 
 from googletrans import Translator
 translator = Translator()
+
+#Import database
+df_stn = pd.read_csv(r'C:\Users\nngna\OneDrive\Documents\MAYDAY_NATI\T4A_GGFORM\db\m_station_db.csv')
+df_stn = df_stn.rename(columns={"name_th":"stn_name_th"})
+df_stn.info()
 
 #Import file
 form_url = 'https://docs.google.com/spreadsheets/d/1qfNAL6iQ_gHhRkUn7QTZ88pjTUGHn2L_eLMQ_9yp_kQ/edit#gid=1736157131'
@@ -37,7 +43,7 @@ print(df.columns)
 #Edit main column header
 df_en.rename({
     "Timestamp":"timestamp",
-    "Name of surveyed station":"stn_name",
+    "Name of surveyed station":"stn_name_th",
     "Name of data recorder":"agent_name",
     "Choose a facility":"r_id",
     "Name the facility.":"acc_name",
@@ -47,21 +53,26 @@ df_en.rename({
     },axis = 1, inplace = True)
 
 print(df_en.columns)
-df_en
+df_en.info()
+df_stn.info()
+
+#Match Station ID
+df_en = pd.merge(df_en,df_stn[['s_id','stn_id']], on=['stn_name_th'])
+
+#Generate unique_response_id
+df_en['res_id'] = df_en['r_id'] + "-" + 
+
 
 #Simplify R Response
 df_en['r_id'] = df_en['r_id'].str.slice_replace(3,repl='')
 print(df_en['r_id'])
-
+df_en
 #Simplify Question Response
+r_ans = []
 for col in df_en.columns:
-    if col.startswith('Q'):
-        for q in df_en[col]:
-            if df_en.q.notnull().values.any():
-                print(df_en[q])
-
+    df_en.loc[df_en.col.isnull(),col] = 0
+        
             
 
 
 df_en
-'''
